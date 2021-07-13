60d8673d86fac840a205586becb4b129ac3aa1fb
farrelleoin93 <farrelleoin93@gmail.com>
2021-07-13 18:12:10 +0000

    Added home app

--- tweleveacres/urls.py
+++ tweleveacres/urls.py
@@ -1,4 +1,4 @@
-"""tweleveacres URL Configuration
+"""acres URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-]
+    path('', include('home.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
