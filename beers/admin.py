1e86f53006cbf4e8d169395af098dffbbda7911f
farrelleoin93 <farrelleoin93@gmail.com>
2021-07-15 14:55:46 +0000

    Customised beer admin

--- beers/admin.py
+++ beers/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import Beer, Category
 
-# Register your models here.
-admin.site.register(Beer)
-admin.site.register(Category)
+
+class BeerAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'description',
+        'abv',
+        'category',
+        'price',
+        'image_url',
+        'image',
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Beer, BeerAdmin)
+admin.site.register(Category, CategoryAdmin)
