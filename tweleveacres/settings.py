e77b47208a6de710991ece6caf7dfe407b4df18d
farrelleoin93 <farrelleoin93@gmail.com>
2021-08-12 00:28:56 +0000

    Added checkout views and templates

--- tweleveacres/settings.py
+++ tweleveacres/settings.py
@@ -52,6 +52,9 @@ INSTALLED_APPS = [
     'bag',
     'findbeers',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +68,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'tweleveacres.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -83,6 +88,10 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
