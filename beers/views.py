2f99c6e15b53e48a2f48f0deb9d4c7ebd8c37f5b
farrelleoin93 <farrelleoin93@gmail.com>
2021-08-20 16:08:59 +0000

    Added search functionality

--- beers/views.py
+++ beers/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Beer
 
 
@@ -6,9 +8,21 @@ def beers(request):
     """ A view to show beers """
 
     beers = Beer.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('beers'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            beers = beers.filter(queries)
 
     context = {
         'beers': beers,
+        'search_term': query,
     }
 
     return render(request, 'beers/beers.html', context)
