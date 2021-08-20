4f7776a361a4ffe11868cded3ffbe019bc991bff
farrelleoin93 <farrelleoin93@gmail.com>
2021-08-20 17:50:41 +0000

    Added category filtering

--- beers/views.py
+++ beers/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Beer
+from .models import Beer, Category
 
 
 def beers(request):
@@ -9,20 +9,27 @@ def beers(request):
 
     beers = Beer.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            beers = beers.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('beers'))
-
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             beers = beers.filter(queries)
 
     context = {
         'beers': beers,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'beers/beers.html', context)
