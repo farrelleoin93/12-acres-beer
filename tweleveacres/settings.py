f520b1e1db96cb13d66d67405ad36254b974aca2
farrelleoin93 <farrelleoin93@gmail.com>
2021-08-20 13:42:52 +0000

    Added confirmation email to terminal

--- tweleveacres/settings.py
+++ tweleveacres/settings.py
@@ -183,6 +183,7 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = '12acres@example.com'
 
 # Google Maps Secret Key
 MAPS_API_KEY = os.environ.get('MAPS_API_KEY', '')
