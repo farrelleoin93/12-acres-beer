3e46d1fb7d402fb631ba150cd5ada2b8d84a2f14
farrelleoin93 <farrelleoin93@gmail.com>
2021-08-29 20:34:57 +0000

    Removed placeholders and added custom labels to forms

--- home/forms.py
+++ home/forms.py
@@ -6,25 +6,17 @@ class ContactForm(forms.Form):
     """
     Contact Form for contact page
     """
-    name = forms.CharField(required=True)
-    from_email = forms.EmailField(required=True)
-    email_subject = forms.CharField(required=True)
-    email_message = forms.CharField(widget=forms.Textarea, required=True)
+    name = forms.CharField(required=True, label='Name')
+    from_email = forms.EmailField(required=True, label='Email')
+    email_subject = forms.CharField(required=True, label='Subject')
+    email_message = forms.CharField(
+        widget=forms.Textarea,
+        required=True, label='Message')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        placeholders = {
-            'name': 'Name',
-            'from_email': 'Your Email',
-            'email_subject': 'Subject',
-            'email_message': 'Message',
-        }
 
         self.fields['name'].widget.attrs['autofocus'] = True
-        for field in self.fields:
-            placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].label = False
 
 
 class NewsletterForm(forms.ModelForm):
