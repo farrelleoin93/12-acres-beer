add16cc31df228cea5df2244aede2d7272255756
farrelleoin93 <farrelleoin93@gmail.com>
2021-08-30 00:39:43 +0000

    Added loading overlay to checkout

---
+++ beers/signals.py
@@ -0,0 +1,22 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import BeerReview
+
+
+@receiver(post_save, sender=BeerReview)
+def update_rating_on_save(sender, instance, created, **kwargs):
+    """
+    Update a beers average rating when a review
+    is created or updated.
+    """
+    instance.beer.calculate_rating()
+
+
+@receiver(post_delete, sender=BeerReview)
+def update_rating_on_delete(sender, instance, **kwargs):
+    """
+    Update a beers average rating when a review is deleted.
+    """
+    if instance.beer:
+        instance.beer.calculate_rating()
