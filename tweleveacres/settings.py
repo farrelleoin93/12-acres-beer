66929a77a923e415409817aa276c10912fb91e5d
farrelleoin93 <farrelleoin93@gmail.com>
2021-08-31 15:31:54 +0000

    Changed name of heroku allowed hosts

--- tweleveacres/settings.py
+++ tweleveacres/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['farrelleoin93-12-acres.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['farrelleoin93-12-acres.herokuapp.com', 'localhost']
 
 
 # Application definition
