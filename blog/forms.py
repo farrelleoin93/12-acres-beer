ac727b677a5bb77c195a873e4d90f9489e4bb328
farrelleoin93 <farrelleoin93@gmail.com>
2021-09-01 01:31:28 +0000

    Added in slug field to form to fix slug bug

--- blog/forms.py
+++ blog/forms.py
@@ -6,7 +6,7 @@ from .models import Post
 class BlogForm(forms.ModelForm):
     class Meta:
         model = Post
-        exclude = ('slug', 'author')
+        exclude = ('author',)
 
     image = forms.ImageField(
         label='Image', required=False,
