53b462d2a685de299758fcf094f4df26a7a21285
farrelleoin93 <farrelleoin93@gmail.com>
2021-09-01 17:54:12 +0000

    Changed email variables

--- tweleveacres/settings.py
+++ tweleveacres/settings.py
@@ -225,7 +225,7 @@ else:
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORDS = os.environ.get('EMAIL_HOST_PASSWORDS')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 # Google Maps Secret Key
