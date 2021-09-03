21a16e16a4b256ac8977b0bf432c4e978ed53918
farrelleoin93 <farrelleoin93@gmail.com>
2021-09-03 02:11:15 +0000

    Added debug = False

--- tweleveacres/settings.py
+++ tweleveacres/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEVELOPMENT') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = ['farrelleoin93-12-acres.herokuapp.com', 'localhost']
 
